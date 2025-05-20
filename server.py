2cd1aeb5ec985db8e5c614df95110899daa1ed7f
Danar Prakosa <d.prakosa@student.unsw.edu.au>
2025-05-21 00:56:48 +0700

    initial commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+CORS(app)
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+@app.route("/message", methods=["POST"])
+def message():
+    data = request.get_json().get("message")
+    print(data)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=[data],
+        config=types.GenerateContentConfig(
+            max_output_tokens=250,
+        ),
+    )
+    return jsonify(response.text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9999)
